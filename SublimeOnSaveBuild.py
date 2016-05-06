9df1949413f5e6d7998e0665de67a185eb083dd8
dxinef <7739224@qq.com>
2016-05-06 13:15:09 +0800

    Add 'filename_ignore' option

--- SublimeOnSaveBuild.py
+++ SublimeOnSaveBuild.py
@@ -1,12 +1,12 @@
 import sublime
 import sublime_plugin
 import re
+import os
 
 
 class SublimeOnSaveBuild(sublime_plugin.EventListener):
     def on_post_save(self, view):
         global_settings = sublime.load_settings(self.__class__.__name__+'.sublime-settings')
-
         # See if we should build. A project level build_on_save setting
         # takes precedence. To be backward compatible, we assume the global
         # build_on_save to be true if not defined.
@@ -15,10 +15,19 @@ class SublimeOnSaveBuild(sublime_plugin.EventListener):
         # Load filename filter. Again, a project level setting takes precedence.
         filename_filter = view.settings().get('filename_filter', global_settings.get('filename_filter', '.*'))
 
+        # filename_ignore
+        filename_ignore = view.settings().get('filename_ignore', global_settings.get('filename_ignore'))
+
         if not should_build:
             return
 
         if not re.search(filename_filter, view.file_name()):
             return
 
+        if filename_ignore and re.search(filename_ignore, os.path.basename(view.file_name())):
+            return
+
         view.window().run_command('build')
+
+        
+        
